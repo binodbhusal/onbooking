53a2c284e1f842edac7bba0c935297b52fc7838b
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-29 14:51:04 +0000

    add gems and update files

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem 'sassc-rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'pry', '~> 0.14.2'
   gem 'rspec-rails', '~> 6.1.0'
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,10 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'factory_bot_rails'
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
