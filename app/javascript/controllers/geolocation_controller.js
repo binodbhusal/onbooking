0e2c3fb3abd70c808bc216b05ffb417c24490d4b
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-03 19:40:03 +0000

    Add geolocation stimulus controller

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from '@hotwired/stimulus'
-import { getDistance } from 'geolib';
+import {  getDistance, convertDistance } from 'geolib';
 
 export default class extends Controller {
+    static targets = ['property']
 connect() {
     console.log('geolib', getDistance)
     console.log('i am geolocation connected')
@@ -10,7 +11,21 @@ connect() {
         console.log(position)
         this.element.dataset.latitude = position.coords.latitude;
         this.element.dataset.longitude = position.coords.longitude;
+        const distance = getDistance (
+            { latitude: position.coords.latitude, longitude: position.coords.longitude },
+            { latitude: 38.7895586, longitude: -9.1650127 }
 
+        )
+        console.log(convertDistance(distance, 'km'))
+        this.propertyTargets.forEach((property) => {
+           
+            const distanceFind = getDistance (
+                { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                { latitude: property.dataset.latitude, longitude: property.dataset.longitude }
+    
+            )
+            property.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFind, 'km'))} kilometers away`
+        })
     })
 }
 }
