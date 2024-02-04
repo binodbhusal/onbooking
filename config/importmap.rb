49e538ee312bd94fb16fd44d94238fb6b48d092a
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-04 12:03:25 +0000

    pinned lodash

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "el-transition" # @0.0.7
 pin "geolib", to: "https://ga.jspm.io/npm:geolib@3.3.4/es/index.js"
-
+pin "lodash-es", to: "https://ga.jspm.io/npm:lodash-es@4.17.21/lodash.js"
