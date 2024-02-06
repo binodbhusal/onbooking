1fc85cd981542e5980df7fc652c9a0840f5e6d78
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-06 21:09:48 +0000

    Add more test cases for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_one(:profile).dependent(:destroy) }
+  it { should have_many(:favroutes).dependent(:destroy) }
+  it { should have_many(:favrouted_properties).through(:favroutes).source(:property) }
 end
