7a29e6a98a8d3f4afd0ae020d7e8088a33e89e9b
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-06 21:18:43 +0000

    update properties factory model

--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :property do
-    name { 'MyString' }
-    headline { 'MyString' }
-    description { 'MyText' }
-    city { 'MyString' }
-    state { 'MyString' }
-    country { 'MyString' }
+    name { 'Mytest' }
+    headline { 'Mytest' }
+    description { 'Mytest' }
+    address1 { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    country { Faker::Address.country }
   end
 end
