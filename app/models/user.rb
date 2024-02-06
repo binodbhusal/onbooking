ca818f339598e03f81071d07685ba1c17a7ad4d0
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-06 21:17:32 +0000

    update user model include after create to add create_profile method for address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_one :profile, dependent: :destroy
-         
-         after_create :create_profile
-         
-         def create_profile
-          self.profile = Profile.new
-          self.save
-         end
+
+  has_one :profile, dependent: :destroy
+  has_many :favroutes, dependent: :destroy
+  has_many :favrouted_properties, through: :favroutes, source: :property
+
+  after_create :create_profile
+
+  def create_profile
+    self.profile = Profile.new
+    save
+  end
 end
