ed79cfa24c751a1e20275496aed5435673cb7533
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-13 13:31:45 +0000

    Add vanila js npm package

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "el-transition" # @0.0.7
 pin "geolib", to: "https://ga.jspm.io/npm:geolib@3.3.4/es/index.js"
 pin "lodash-es", to: "https://ga.jspm.io/npm:lodash-es@4.17.21/lodash.js"
+pin "vanillajs-datepicker", to:"https://ga.jspm.io/npm:vanillajs-datepicker@1.3.4/js/main.js"
