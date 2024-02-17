39645e30cdb2569dcccf588a91b4c3c25ebee6e9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-17 23:19:42 +0000

    Add contriable concerns

---
+++ app/models/concerns/contriable.rb
@@ -0,0 +1,8 @@
+module Contriable
+def country_name
+    country = ISO3166::Country[country_code]
+    return country.name if country.present? && country.respond_to?(:name)
+    
+    'Portugal'
+    end
+end
