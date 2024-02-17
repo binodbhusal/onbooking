492c411a2ab6918e05d0565206837d7ab69d2cd8
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-17 23:16:57 +0000

    Add accounts controller and rspec spec test

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,17 @@
+class AccountsController < ApplicationController
+    before_action :authenticate_user!
+    def show
+        @user = current_user
+    end
+    def update
+        @user = current_user
+        @user.update(account_params)
+        redirect_to account_path(@user)
+        flash[:notice]= "Email account updated successfully"
+
+    end
+    private 
+   def account_params
+    params.require(:account).permit(:email)
+   end
+end
---
+++ spec/requests/accounts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Accounts', type: :request do
+    let (:user) {create(:user)}
+
+    before {sign_in user}
+    describe 'GET show' do
+    it 'succeds' do
+        get account_path(user)
+        expect(response).to be_successful
+    end
+    end
+    describe 'PUT update' do
+        it 'succeeds' do
+         user.update! email:"binod@gmail.com"
+          put account_path(user), params: {
+           account: {
+             email:"bhusal@gmail.com"
+            }
+          }
+          expect(user.reload.email).to eq("bhusal@gmail.com")
+          expect(response).to be_redirect
+        end
+      end
+end
