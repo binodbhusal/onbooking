20565ab7cf57f2f3968290ee44f506e2d2a51ded
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-17 23:21:55 +0000

    Add pundit policies

---
+++ app/policies/profile_policy.rb
@@ -0,0 +1,13 @@
+class ProfilePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  
+  def update?
+    user.profile ==record
+    end
+end
