1e1635e65f354fa49ffa7f9a600aabd46bea576f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-18 13:25:39 +0000

    Include payment controller

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,6 @@
+class PaymentsController < ApplicationController
+    before_action :authenticate_user!
+    def index 
+        @payments = current_user.payments.includes(reservation: :property)
+    end
+end
