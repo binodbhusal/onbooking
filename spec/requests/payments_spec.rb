acbbf199ddd71496ad476f8ee016ee109ead15ba
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-18 13:26:52 +0000

    Add payments rspec spec

---
+++ spec/requests/payments_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Passwords', type: :request do
+  let(:user) { create(:user) }
+    let (:payments) {create_list(:payments)}
+  before { sign_in user }
+
+  describe 'GET show' do
+    it 'succeeds' do
+      get payments_path
+      expect(response).to be_successful
+    end
+  end
+
+  
+end
