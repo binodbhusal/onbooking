bd11dfef1271b2dd769ec86a5e4287e721dc6275
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-20 21:50:44 +0000

    Modify countryable concern and user reference migration

--- app/models/concerns/countryable.rb
+++ app/models/concerns/countryable.rb
@@ -1,6 +1,6 @@
 module Countryable
   def country_name
     country = ISO3166::Country[country_code]
-    country.translations[I18n.locale.to_s] || country.common_name || country.iso_short_name
+    return country.translations[I18n.locale.to_s] || country.common_name || country.iso_short_name if country&.respond_to?(:translations)
   end
 end
