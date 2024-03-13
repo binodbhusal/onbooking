8db81ae770c0fe40987e62e82b568b0c1c532d29
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-13 19:37:44 +0000

    Add user host image to properties show page and update review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,3 +3,6 @@ module ApplicationHelper
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'
   end
 end
+def profile_image
+  'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_1280.png'
+end
