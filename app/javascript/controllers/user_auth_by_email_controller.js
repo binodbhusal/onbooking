0842ee10e7e5150a0b9f62a2828b9d87926b74cf
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-15 14:46:59 +0000

    Add devise login path in js controller

--- app/javascript/controllers/user_auth_by_email_controller.js
+++ app/javascript/controllers/user_auth_by_email_controller.js
@@ -1,42 +1,74 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-    static targets = ['emailInput', 'btnSubmit', 'emailContainer','errorSvg', 'errorMessage']
-    connect() {
-        console.log('i am connected')
-       this.emailInputTarget.addEventListener('input',() => {
-        this.emailContainerTarget.classList.remove('invalid-email-input-field')
-        this.errorSvgTarget.classList.add('hidden')
-        this.errorMessageTarget.classList.add('hidden')
-       })
-        this.btnSubmitTarget.addEventListener('click', async (e) => {
-            e.preventDefault()
-            if (this.emailInputTarget.value.length===0) {
-                this.emailContainerTarget.classList.add('invalid-email-input-field')
-                this.errorSvgTarget.classList.remove('hidden')
-                this.errorMessageTarget.classList.remove('hidden')
+    static targets = ['emailInput', 'passInput', 'btnSubmit', 'emailContainer', 'errorSvg', 'errorMessage', 'passContainer','passErrorMessage', 'passerrorSvg',];
 
-            }else {
-                const url = `/api/users_by_email?email=${encodeURIComponent(this.emailInputTarget.value)}`
-                const headers = {
-                    'Accept': 'application/json'
-                };
-                try {
-                    const response = await fetch(url, {headers})
-                    if(response.ok){
-                        Turbo.visit('/users/sign_in')
-                    }else{
-                        Turbo.visit('/users/sign_up')
-                    }
-                } catch (error) {
-                    throw new Error('Error fetching:',error)
-                }
-            }
+    connect() {
+        console.log('I am connected');
+
+        this.emailInputTarget.addEventListener('input', () => {
+            this.emailContainerTarget.classList.remove('invalid-email-input-field');
+            this.errorSvgTarget.classList.add('hidden');
+            this.errorMessageTarget.classList.add('hidden');
+        });
+        this.passInputTarget.addEventListener('input', () => {
+            this.passContainerTarget.classList.remove('invalid-email-input-field')
+            this.passerrorSvgTarget.classList.add('hidden')
+            this.passErrorMessageTarget.classList.add('hidden')
         })
+        this.btnSubmitTarget.addEventListener('click', async (e) => {
+            e.preventDefault();
+
+            const email = this.emailInputTarget.value;
+            const password = this.passInputTarget.value;
+
+            if (email.length === 0) {
+                this.emailContainerTarget.classList.add('invalid-email-input-field');
+                this.errorSvgTarget.classList.remove('hidden');
+                this.errorMessageTarget.classList.remove('hidden');
+                return;
+            }
+            if (password.length===0) {
+                this.passContainerTarget.classList.add('invalid-email-input-field');
+                this.passErrorMessageTarget.classList.remove('hidden');
+                this.passerrorSvgTarget.classList.remove('hidden');
+                return;
+            }
+            const url = '/users/sign_in';
+            const data = {
+                user: {
+                    email: email,
+                    password: password
+                }
+            };
+            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+            console.log('csrf token', csrfToken)
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRF-Token': csrfToken
+            };
+
+            try {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: headers,
+                    body: JSON.stringify(data)
+                });
+
+                if (response.ok) {
+                    Turbo.visit('/');
+                } else {
+                    const responseData = await response.json();
+                    if(responseData.error){
+                        const errorMessageElement = document.querySelector('.error-message')
+                        errorMessageElement.textContent= responseData.error
+                        errorMessageElement.classList.remove('hidden')
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching:', error);
+            }
+        });
     }
 }
-                
-                   
-
-
-       
